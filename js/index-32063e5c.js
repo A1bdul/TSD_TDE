9e2adae98cebe5cd5300a3eb3ad2ae700c11df06
Vince <73336016+A1bdul@users.noreply.github.com>
2024-08-13 23:49:52 +0100

    Update index-32063e5c.js

--- js/index-32063e5c.js
+++ js/index-32063e5c.js
@@ -10536,7 +10536,7 @@ const Ra = $3({
         me(
             "p",
             { class: "broke-text text-center px-4 py-4 lg:px-72 lg:py-8" },
-            "vote 4 pojak 2024",
+            "vote 4 Pojak",
             -1,
         
     )),
